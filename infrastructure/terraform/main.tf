730034abae31233daf75ae3181ee86f18b59ec32
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-07-05 22:34:11 +0900

    cloudfront s3 distribution

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -25,3 +25,65 @@ resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block"
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+    origin_id   = "S3-nextjs-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-nextjs-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for Next.js app bucket"
+}
+
+resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
+  bucket = aws_s3_bucket.nextjs_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oai.id}"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.nextjs_bucket.arn}/*"
+      }
+    ]
+  })
+}
